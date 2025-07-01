0e46533ebeaa308ed5fec26d2632e67a9a9b1341
SAN_DESH63 <sandeshsharma924@gmail.com>
2025-07-01 20:21:12 +0545

    feat: update package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flipshelf"
+    namespace = "com.flipshelfdev.flipshelf"
     compileSdk = flutter.compileSdkVersion
     //ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flipshelf"
+        applicationId = "com.flipshelfdev.flipshelf"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/flipshelfdev/flipshelf/MainActivity.kt
+++ android/app/src/main/kotlin/com/flipshelfdev/flipshelf/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flipshelf
+package com.flipshelfdev.flipshelf
 
 import io.flutter.embedding.android.FlutterActivity
 
